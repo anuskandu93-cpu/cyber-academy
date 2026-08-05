3fdb071bc9a0c1f65634ff9713567d2a699c987a
anuskandu93-cpu <anuskandu93@gmail.com>
2026-08-05 14:43:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function commencerMission() {
+
+    alert("Bienvenue dans la Cyber Academy !");
+
+    document.getElementById("message").innerHTML =
+    "✅ Mission lancée ! Tu es maintenant un futur analyste cybersécurité.";
+
+    document.getElementById("message").style.color = "cyan";
+
+}
+function testerAge() {
+
+    let age = prompt("Quel âge as-tu ?");
+
+    if (age >= 16) {
+        document.getElementById("resultat").innerHTML =
+        "✅ Accès autorisé à la Cyber Academy !";
+    } else {
+        document.getElementById("resultat").innerHTML =
+        "❌ Désolé, il faut avoir au moins 16 ans.";
+    }
+
+}
+function lancerMission(texte) {
+    document.getElementById("mission").innerHTML = texte;
+}
